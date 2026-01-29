2cac61f8c5988d5dca12f22cb5459d54ac6d3b1a
Lord-Y <Lord-Y@users.noreply.github.com>
2026-01-29 13:11:18 +0100

    enhancements(grpc): Add debug log and remove fmt debug in test

--- cluster/grpc_responses.go
+++ cluster/grpc_responses.go
@@ -9,6 +9,7 @@ func (c *Cluster) respServicePortsDiscovery(data RPCResponse) {
 
 	response := data.Response.(RPCServicePortsDiscoveryResponse)
 	if response != (RPCServicePortsDiscoveryResponse{}) {
+		c.logger.Debug().Msgf("discovery response address %s id %s node pool %s http port %d grpc port %d raft port %d\n", response.Address, response.ID, response.NodePool, response.PortHTTP, response.PortGRPC, response.PortRaft)
 		c.mu.Lock()
 		if _, ok := c.nodeMap[response.ID]; !ok {
 			c.bootstrapExpectedSize.Add(1)
