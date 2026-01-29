a8895e1de00cc895146cbf2c7685f2653f974aa7
Lord-Y <Lord-Y@users.noreply.github.com>
2026-01-29 13:06:24 +0100

    chore(ci): Add coverpkg into golang unit testing command

--- scripts/unit_testing.sh
+++ scripts/unit_testing.sh
@@ -4,7 +4,7 @@ COVERAGE=/tmp/coverage.out
 cd /home/vagrant/scalezilla
 go mod download
 
-go test -v -race ./... -coverprofile=${COVERAGE}
+go clean -cache && go test -v -race ./... -coverpkg=./... -coverprofile=${COVERAGE}
 sed -i '/scalezilla\/scalezilla\/scalezillapb/d' ${COVERAGE}
 go tool cover -func ${COVERAGE}
 
