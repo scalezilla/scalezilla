5f03ff21b72e4d89f83be2661f15e865c70e3aa9
Lord-Y <Lord-Y@users.noreply.github.com>
2026-02-09 21:29:59 +0100

    chore(scripts): Run code coverage only when tests are successful

--- scripts/unit_testing.sh
+++ scripts/unit_testing.sh
@@ -5,6 +5,12 @@ cd /home/vagrant/scalezilla
 go mod download
 
 go clean -cache && go test -v -race ./... -coverpkg=./... -coverprofile=${COVERAGE}
+if [ $? == 0 ]
+then
+  RUN=true
+fi
 sed -i '/scalezilla\/scalezilla\/scalezillapb/d' ${COVERAGE}
-go tool cover -func ${COVERAGE}
-
+if [ "${RUN}" == "true" ]
+then
+  go tool cover -func ${COVERAGE}
+fi
