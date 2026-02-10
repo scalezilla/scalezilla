46473deb2de763eb19ecf2da7c25326e564b47c0
Lord-Y <Lord-Y@users.noreply.github.com>
2026-02-11 00:08:20 +0100

    chore(scripts): Reset scalezilla debug log after each vagrant provision command

--- scripts/scalezilla.sh
+++ scripts/scalezilla.sh
@@ -12,10 +12,12 @@ fi
 shift
 
 SCALEZILLA_HOME_DIR=/home/vagrant/scalezilla
+SCALEZILLA_TMP_FILE=/tmp/scalezilla.log
+> ${SCALEZILLA_TMP_FILE}
 cd ${SCALEZILLA_HOME_DIR}
 go mod download
 pgrep go && kill $(pgrep -f "main agent") && sleep 2
 rm -rf /var/lib/scalezilla
 export SCALEZILLA_LOG_LEVEL=trace
-nohup go run main.go agent config -f ${SCALEZILLA_HOME_DIR}/cluster/testdata/config/${CONFIG} &> /tmp/scalezilla.log &
+nohup go run main.go agent config -f ${SCALEZILLA_HOME_DIR}/cluster/testdata/config/${CONFIG} &> ${SCALEZILLA_TMP_FILE} &
 
