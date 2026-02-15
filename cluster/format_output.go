68e59d5fb3f911dd7c506aaa233160335546de16
Lord-Y <Lord-Y@users.noreply.github.com>
2026-02-15 14:49:33 +0100

    fix(cluster): Fix golangci err static check for printTableNodesList func

--- cluster/format_output.go
+++ cluster/format_output.go
@@ -18,16 +18,16 @@ func printTableNodesList(body []byte) {
 
 	// tabwriter aligns columns using tabs
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
-	fmt.Fprintln(w, "ID\tNAME\tADDRESS\tKIND\tLEADER\tNODEPOOL")
+	_, _ = fmt.Fprintln(w, "ID\tNAME\tADDRESS\tKIND\tLEADER\tNODEPOOL")
 
 	for _, n := range nodes {
 		leader := "false"
 		if n.Leader {
 			leader = "true"
 		}
-		fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%s\n",
+		_, _ = fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%s\n",
 			n.ID, n.Name, n.Address, strings.ToLower(n.Kind), leader, n.NodePool,
 		)
 	}
-	w.Flush()
+	_ = w.Flush()
 }
