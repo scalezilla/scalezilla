5c9228e4fd87cb47b89fd43e5f291e935125cda8
Lord-Y <Lord-Y@users.noreply.github.com>
2026-03-12 14:17:05 +0100

    chore(scripts): Add containerd requirements
    Related to github issue #23

--- scripts/install_redhat_os_package.sh
+++ scripts/install_redhat_os_package.sh
@@ -3,9 +3,42 @@
 #dnf update -yq
 dnf install -yq vim wget git nc \
   bind-utils net-tools traceroute jq gcc \
-  uuid-runtime
+  uuid-runtime containerd
 
 # set default shell to bash
 which chsh || dnf install -yq util-linux-user
 grep vagrant /etc/passwd | grep -q /bin/bash || chsh -s /bin/bash vagrant
 
+OS=$(uname -s | tr -s "A-Z" "a-z")
+OS_ARCH=$(uname -m)
+if [[ "${OS}" == "linux" ]]
+then
+  if [[ "${OS_ARCH}" == "aarch64" ]]
+  then
+    ARCH=arm64
+  else
+    ARCH=amd64
+  fi
+fi
+
+# https://github.com/opencontainers/runc/releases
+RUNC_VERSION=1.4.0
+wget -qO /usr/local/sbin/runc https://github.com/opencontainers/runc/releases/download/v${RUNC_VERSION}/runc.${ARCH}
+chmod +x /usr/local/sbin/runc
+
+mkdir -p /opt/cni/bin
+# https://github.com/containernetworking/plugins/releases
+CNI_VERSION=1.9.0
+wget -P /tmp -q https://github.com/containernetworking/plugins/releases/download/v${CNI_VERSION}/cni-plugins-linux-${ARCH}-v${CNI_VERSION}.tgz
+tar Cxzvf /opt/cni/bin /tmp/cni-plugins-linux-${ARCH}-v${CNI_VERSION}.tgz
+rm -f /tmp/cni-plugins-linux-${ARCH}-v${CNI_VERSION}.tgz
+
+# https://github.com/kubernetes-sigs/cri-tools/releases
+CRICTL_VERSION=v1.35.0
+wget -P /tmp -q https://github.com/kubernetes-sigs/cri-tools/releases/download/$CRICTL_VERSION/crictl-$CRICTL_VERSION-linux-${ARCH}.tar.gz
+sudo tar zxvf /tmp/crictl-$CRICTL_VERSION-linux-${ARCH}.tar.gz -C /usr/local/bin
+rm -f /tmp/crictl-$CRICTL_VERSION-linux-${ARCH}.tar.gz
+cat > /etc/crictl.yaml <<EOF
+runtime-endpoint: unix:///run/containerd/containerd.sock
+image-endpoint: unix:///run/containerd/containerd.sock
+EOF
